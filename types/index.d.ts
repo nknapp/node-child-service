ccebd86f56817258319135e01c09864ac7d4dc47
Nils Knappmeier <github@knappi.org>
2020-06-28 12:07:28 +0200

    feature: add typings

---
+++ types/index.d.ts
@@ -0,0 +1,31 @@
+declare module "child-service" {
+	export class ChildService {
+		constructor(userOptions?: ChildServiceOptions);
+
+		start(): Promise<void>;
+
+		stop(): Promise<void>;
+	}
+
+	interface ChildServiceOptions {
+		command: string;
+		args?: string[];
+		readyRegex?: RegExp;
+		outputLimit?: number;
+		spawnOptions?: AllowedSpawnOptions;
+		timeoutAfterSignal?: number;
+	}
+
+	interface AllowedSpawnOptions {
+		cwd?: string;
+		env?: ProcessEnv;
+	}
+
+	// Copied from the node.js type definitions
+	interface ProcessEnv extends Dict<string> {}
+
+	// Copied from the node.js type definitions
+	interface Dict<T> {
+		[key: string]: T | undefined;
+	}
+}
---
+++ types/test.ts
@@ -0,0 +1,55 @@
+import { ChildService } from "child-service";
+
+function simpleTest() {
+	new ChildService({
+		command: "executable",
+	});
+}
+
+function withArgs() {
+	new ChildService({
+		command: "executable",
+		args: ["some", "arguments"],
+		spawnOptions: {
+			cwd: "workdir",
+			env: {
+				VARIABLE1: "stringvalue1",
+				VARIABLE2: "stringvalue2",
+			},
+		},
+	});
+}
+
+function withSpawnOptions() {
+	new ChildService({
+		command: "executable",
+		spawnOptions: {
+			cwd: "workdir",
+			env: {
+				VARIABLE1: "stringvalue1",
+				VARIABLE2: "stringvalue2",
+			},
+		},
+	});
+}
+
+function withReadyRegex() {
+	new ChildService({
+		command: "executable",
+		readyRegex: / Connected /,
+	});
+}
+
+function withTimeout() {
+	new ChildService({
+		command: "executable",
+		timeoutAfterSignal: 3000,
+	});
+}
+
+function withOutputLimit() {
+	new ChildService({
+		command: "executable",
+		outputLimit: 10000,
+	});
+}
